e82f1446672923eb0ff2f43a2bc6638ddcf0856f
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-11 23:39:31 -0300

    Aula assistida até 7:44

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -4,10 +4,10 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import fastify from 'fastify'
-import { createGoal } from '../functions/create-goal'
-import z from 'zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createGoalRoute } from './routes/create-goal'
+import { createGoalCompletionRoute } from './routes/create-completion'
+import { getWeekPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -17,51 +17,32 @@ app.setSerializerCompiler(serializerCompiler)
 /*
 Criando rotas, depois vai ser dividido em arquivos e vou manter isso aqui apenas para poder
 ficar como exemplo
+
+  app.post(
+    '/goals',
+    {
+      schema: {
+        body: z.object({
+          title: z.string(),
+          desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        }),
+      },
+    },
+    async request => {
+      const { title, desiredWeeklyFrequency } = request.body
+
+      await createGoal({
+        title,
+        desiredWeeklyFrequency,
+      })
+    }
+  )
 */
 
-app.get('/pending-goals', async () => {
-  const { pendingGoals } = await getWeekPendingGoals()
-
-  return { pendingGoals }
-})
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    })
-  }
-)
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-
-    await createGoalCompletion({
-      goalId,
-    })
-  }
-)
+app.register(createGoalRoute)
+app.register(createGoalCompletionRoute)
+app.register(getWeekPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 /*
 Server é quem vai rodar a aplicação no BackEnd
