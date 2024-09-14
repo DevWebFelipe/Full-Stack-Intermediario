a482980b10958fd2d0d3f94e5f8eaab7f8630f83
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2024-09-14 18:27:21 -0300

    Correção problena na rota POST

--- web/src/http/create-goal-completeion.ts
+++ web/src/http/create-goal-completeion.ts
@@ -2,7 +2,7 @@ export async function createGoalCompletion(goalId: string) {
   await fetch('http://localhost:3333/completions', {
     method: 'POST',
     headers: {
-      'Content-Tipe': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       goalId,
